f02ff0770a0c90cbd7e6808b429df6208d21c9bb
Janio <janiojunior@gmail.com>
2025-08-30 10:21:01 -0300

    adicionando a table do material

--- src/app/components/estado/estado-list/estado-list.component.ts
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Estado } from '../../../models/estado.model';
+import { EstadoService } from '../../../services/estado.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-estado-list',
-  imports: [],
+  imports: [MatTableModule, MatInputModule, MatFormFieldModule],
   templateUrl: './estado-list.component.html',
   styleUrl: './estado-list.component.css'
 })
-export class EstadoListComponent {
+export class EstadoListComponent implements OnInit {
+  displayedColumns: string[] = ['numero', 'nome', 'sigla'];
+  estados: Estado[] = [];
+  dataSource = new MatTableDataSource(this.estados);
+
+  constructor(private estadoService: EstadoService) {
+
+  }
+
+  ngOnInit(): void {
+    this.estadoService.getEstados().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
